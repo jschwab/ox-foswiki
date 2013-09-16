bba373307dae23d2208b1e51bbcb344efe6a593b
Josiah Schwab <jschwab@gmail.com>
2013-09-16 12:00:18 -0700

    added minimal emacs config

---
+++ minimal-org.el
@@ -0,0 +1,16 @@
+;;; Minimal setup to load latest `org-mode'
+
+;; activate debugging
+(setq debug-on-error t
+      debug-on-signal nil
+      debug-on-quit nil)
+
+;; add latest org-mode to load path
+(add-to-list 'load-path (expand-file-name "/home/jschwab/Software/org-mode/lisp"))
+(add-to-list 'load-path (expand-file-name "/home/jschwab/Software/org-mode/contrib/lisp" t))
+
+;; add foswiki
+(add-to-list 'load-path (expand-file-name "/home/jschwab/Software/ox-foswiki" t))
+
+
+
